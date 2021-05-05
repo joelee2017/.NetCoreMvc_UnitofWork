6bc64ef02b920dd5b20805668369927aab351670
joe <as>
2021-05-05 16:31:45 +0800

    換一種寫法測試，尚未驗證

--- MvcMovie/Model/UnitOfWork/UnitOfWork.cs
+++ MvcMovie/Model/UnitOfWork/UnitOfWork.cs
@@ -8,19 +8,38 @@ namespace Model.UnitOfWork
 {
     public class UnitOfWork : IDisposable, IUnitOfWork
     {
-        private MvcMovieContext _context;
+        //private MvcMovieContext _context;
+        private readonly DbContext _context;
         private Hashtable repositories = new Hashtable();
 
-        public UnitOfWork(MvcMovieContext context)
+        public UnitOfWork(DbContext context)
         {
             _context = context;
         }
 
         public IGenericRepository<T> GetRepository<T>() where T : class
         {
-            if (!repositories.Contains(typeof(T)))
+            //if (!repositories.Contains(typeof(T)))
+            //{
+            //    repositories.Add(typeof(T), new GenericRepository<T>(_context));
+            //}
+
+            if (repositories == null)
             {
-                repositories.Add(typeof(T), new GenericRepository<T>(_context));
+                repositories = new Hashtable();
+            }
+
+            var type = typeof(T).Name;
+
+            if (!repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+
+                var repositoryInstance =
+                    Activator.CreateInstance(repositoryType
+                            .MakeGenericType(typeof(T)), _context);
+
+                repositories.Add(type, repositoryInstance);
             }
             return (IGenericRepository<T>)repositories[typeof(T)];
         }
